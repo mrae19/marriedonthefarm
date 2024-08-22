a3ca87278e63406119ecb863e14b2553980bdcf2
Matt Rae <matt@mattrae.ca>
2024-08-21 22:13:45 -0400

    Updates!

--- js/main.js
+++ js/main.js
@@ -281,7 +281,7 @@
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
-  let birthday = "Dec 15, 2023 00:00:00",
+  let birthday = "Aug 9, 2025 00:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
         let now = new Date().getTime(),
